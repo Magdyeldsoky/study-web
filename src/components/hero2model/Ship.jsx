2dc76c052e4e9c8136edbc3910c8910460a4fbcf
Magdy Mohamed Eldsoky <youssifmohamed5590@gmail.com>
2026-02-07 22:57:08 +0200

    update

--- src/components/hero2model/Ship.jsx
+++ src/components/hero2model/Ship.jsx
@@ -1,46 +1,40 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useGLTF } from '@react-three/drei'
 import * as THREE from 'three'
 
 export function Ship(props) {
     const { nodes, materials } = useGLTF('/study-web/models/ship.glb')
 
-    const metallicMaterial = new THREE.MeshPhysicalMaterial({
-        color: "#222222",
-        metalness: 1,
-        roughness: 0.15,
-        reflectivity: 1,
-        clearcoat: 1,
-        clearcoatRoughness: 0.1,
-    })
-
-    const glowMaterial = new THREE.MeshStandardMaterial({
-        color: "#0077ff",
-        emissive: "#0033ff",
-        emissiveIntensity: 10,
-        toneMapped: false,
-    })
-
-    const darkDetailMaterial = new THREE.MeshStandardMaterial({
-        color: "#050505",
-        roughness: 0.8,
-        metalness: 0.2,
-    })
+    useMemo(() => {
+        if (materials.B_Mat) {
+            materials.B_Mat.color.set("#050505");
+            materials.B_Mat.metalness = 1;
+            materials.B_Mat.roughness = 0.2;
+        }
+        if (materials.M_Mat) {
+            materials.M_Mat.color.set("#080808");
+            materials.M_Mat.metalness = 1;
+            materials.M_Mat.roughness = 0.1;
+        }
+        if (materials.F_Mat) {
+            materials.F_Mat.emissive.set("#0066ff");
+            materials.F_Mat.emissiveIntensity = 10;
+            materials.F_Mat.toneMapped = false;
+        }
+    }, [materials]);
 
     return (
         <group {...props} dispose={null}>
             <group rotation={[-Math.PI / 2, 0, 0]} scale={0.022}>
                 <group position={[-272.294, -452.838, -101.812]}>
-                    <mesh geometry={nodes.Object_4.geometry} material={metallicMaterial} />
-
-                    <mesh geometry={nodes.Object_3.geometry} material={darkDetailMaterial} />
-
-                    <mesh geometry={nodes.Object_5.geometry} material={glowMaterial} />
-                    <mesh geometry={nodes.Object_6.geometry} material={glowMaterial} />
-                    <mesh geometry={nodes.Object_7.geometry} material={glowMaterial} />
-                    <mesh geometry={nodes.Object_8.geometry} material={glowMaterial} />
-                    <mesh geometry={nodes.Object_9.geometry} material={glowMaterial} />
-                    <mesh geometry={nodes.Object_10.geometry} material={glowMaterial} />
+                    <mesh geometry={nodes.Object_3.geometry} material={materials.B_Mat} />
+                    <mesh geometry={nodes.Object_4.geometry} material={materials.M_Mat} />
+                    <mesh geometry={nodes.Object_5.geometry} material={materials.F_Mat} />
+                    <mesh geometry={nodes.Object_6.geometry} material={materials.F_Mat} />
+                    <mesh geometry={nodes.Object_7.geometry} material={materials.F_Mat} />
+                    <mesh geometry={nodes.Object_8.geometry} material={materials.F_Mat} />
+                    <mesh geometry={nodes.Object_9.geometry} material={materials.F_Mat} />
+                    <mesh geometry={nodes.Object_10.geometry} material={materials.F_Mat} />
                 </group>
             </group>
         </group>
